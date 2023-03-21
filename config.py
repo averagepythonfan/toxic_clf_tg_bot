232ca91d92f85c2292443788b072d7967cd2afd9
averagepythonfan <djonvorobei@bk.ru>
2023-03-21 20:38:37 +0300

    add toxicity level, help command and minor fixes

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 TOKEN: str = os.getenv('TOKEN')
 FASTAPI_ADDRESS: str = os.getenv('FASTAPI_ADDRESS')
+TOXICITY_LEVEL: float = float(os.getenv('TOXICITY_LEVEL'))
--- handlers/handlers.py
+++ handlers/handlers.py
@@ -7,7 +7,8 @@ import aiohttp
 import logging
 from aiogram import types, Router
 from aiogram.filters import Command, Text
-from config import FASTAPI_ADDRESS
+from config import FASTAPI_ADDRESS, TOXICITY_LEVEL
+
 
 API_URL = FASTAPI_ADDRESS
 headers = {
@@ -15,25 +16,30 @@ headers = {
     'Content-Type': 'application/json',
 }
 
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-help_str = '''HELP'''
+
+help_str = '''Чат-бот для определения токсичности в сообщениях.
+Добавьте бота в свой чат, дайте ему права администратора и разрешите ему удалять сообщения.
+Также для бота доступна команда "/check (ваще сообщение)" для вывода результатов проверки.'''
 
 
 async def help_command(message: types.Message):
-    '''Help command.
+    '''Обработчик для команды помощи.
 
     :usage: /help'''
     await message.answer(help_str)
 
 
 async def main_handler(message: types.Message):
-    '''Main handler for toxicity classification
+    '''Главный обработчик сообщений для классификации токсичности
 
-    :params: message - any text.
-    If toxic score greater then 0.9 it classified as toxic message.
-    And then bot delete the message.
+    :params: message - любой текст на русском.
+
+    Если показатель токсичности выше 2.5,
+    то сообщение определяется как неприемлемое и удаляется ботом.
     '''
     try:
         msg = str(message.text)
@@ -41,7 +47,7 @@ async def main_handler(message: types.Message):
         async with aiohttp.ClientSession() as session:
             async with session.post(API_URL, headers=headers, json=playload) as resp:
                 text = await resp.json()
-                if text['response']['toxicity'] > 2.5:
+                if text['response']['toxicity'] > TOXICITY_LEVEL:
                     await message.reply("It's toxic!")
                     await message.delete()
         logger.debug(f'User {message.from_user.id} send {message.text}. Response {text["response"]}')
@@ -50,7 +56,10 @@ async def main_handler(message: types.Message):
 
 
 async def check_value_command(message: types.Message):
-    '''Check value of toxicity in message.
+    '''Обработчик для команды /check. Возвращает результаты проверки.
+
+    :params: message - любой текст на русском.
+    :usage: /check (ваш текст)
     '''
     playload = { 'input': message.text[6:] }
     async with aiohttp.ClientSession() as session:
@@ -58,6 +67,7 @@ async def check_value_command(message: types.Message):
             text = await resp.json()
             await message.reply(str(text['response']))
     logger.debug(f'User {message.from_user.id} send {message.text[6:]}. Response {text["response"]}')
+
 
 def register_message_handlers(router: Router):
     router.message.register(help_command, Command(commands=['help', 'start']))
